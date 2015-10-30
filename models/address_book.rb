bfee2f96d56e7b05b525dd57781f4ff83e89905e
CharlesMassey <chasemassey@gmail.com>
2015-10-29 22:15:09 -0400

    Assignement 19 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,27 +1,31 @@
-require_relative 'Entry'
+require_relative 'entry'
  
- class AddressBook
-   attr_accessor :entries
+class AddressBook
+  attr_accessor :entries
 
    def initialize
      @entries = []
    end
-   
-   def add_entry(name, phone_number, email)
- # #9
+
+    def remove_entry(name, phone_number, email)
+      delete_entry = nil
+
+      @entries.each do |entry|
+      if name == entry.name && phone_number == entry.phone_number && email == entry.email 
+        delete_entry = entry
+      end
+    end
+      @entries.delete(delete_entry)
+    end
+
+    def add_entry(name, phone_number, email)
      index = 0
      @entries.each do |entry|
- # #10
-       if name < entry.name
-         break
-       end
-       index += 1
-     end
- # #11
-     @entries.insert(index, Entry.new(name, phone_number, email)
-   end
-
-     def remove_entry(name, phone_number, email)
-    # Implementation goes here
-  	 end
- end
+      if name < entry.name
+       break
+      end
+      index += 1
+    end
+     @entries.insert(index, Entry.new(name, phone_number, email))
+  end
+end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -1,54 +1,56 @@
 require_relative '../models/address_book'
 
- RSpec.describe AddressBook do  
-   describe "attributes" do
-     it "should respond to entries" do
+RSpec.describe AddressBook do  
+  describe "attributes" do
+ 	  it "should respond to entries" do
        book = AddressBook.new
        expect(book).to respond_to(:entries)
-     end
+    end
 
-     it "should initialize entries as an array" do
+    it "should initialize entries as an array" do
        book = AddressBook.new
        expect(book.entries).to be_a(Array)
-     end
+    end
 
-     it "should initialize entries as empty" do
+    it "should initialize entries as empty" do
        book = AddressBook.new
        expect(book.entries.size).to eq(0)
-     end
+    end
    end
 
-   describe "#add_entry" do
-     it "adds only one entry to the address book" do
-       book = AddressBook.new
-       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+  describe "#add_entry" do
+    it "adds only one entry to the address book" do
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
  
-       expect(book.entries.size).to eq(1)
-     end
+      expect(book.entries.size).to eq(1)
+    end
  
-     it "adds the correct information to entries" do
-       book = AddressBook.new
-       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-       new_entry = book.entries[0]
+    it "adds the correct information to entries" do
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      new_entry = book.entries[0]
  
-       expect(new_entry.name).to eq('Ada Lovelace')
-       expect(new_entry.phone_number).to eq('010.012.1815')
-       expect(new_entry.email).to eq('augusta.king@lovelace.com')
-     end
+      expect(new_entry.name).to eq('Ada Lovelace')
+      expect(new_entry.phone_number).to eq('010.012.1815')
+      expect(new_entry.email).to eq('augusta.king@lovelace.com')
+    end
+ 	end 
 
-     describe "#remove entry" do 
-     	it "remove an entry using the name, phone_number, and email_address"
+  describe "#remove entry" do 
+    it "remove an entry using the name, phone_number, and email_address" do
      	book = AddressBook.new
      	book.add_entry('Chase Massey', '478.251.0820', 'chasemassey@gmail.com')
 
-     	name = 'Ada Lovelace'
-     	phone_number = '010.012.1815'
-     	email_address = 'augusta.king@lovelace.com'
+	     name = 'Ada Lovelace'
+	     phone_number = '010.012.1815'
+	     email_address = 'augusta.king@lovelace.com'
+       book.add_entry(name, phone_number, email_address)
 
-     	expect(book.entries.size).to eq 2
-     	book.remove_entry(name, phone_number, email_address)
-     	expect(book.entries.size).to eq 1
-     	expect(book_entries.first.name).to eq("Chase Massey")
-     end
-   end
- end
+	     expect(book.entries.size).to eq 2
+	     book.remove_entry(name, phone_number, email_address)
+	     expect(book.entries.size).to eq 1
+	     expect(book.entries.first.name).to eq("Chase Massey")
+    end
+  end 
+end
